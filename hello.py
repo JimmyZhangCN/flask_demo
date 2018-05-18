6d4dcdb34b6d151cfaa60c092a46fdbbdd895c0d
JimmyZhang <JimmyZhangCDCD@outlook.com>
2018-05-18 16:02:30 +0800

    add a demo template

--- hello.py
+++ hello.py
@@ -1,11 +1,25 @@
 from flask import Flask
+from flask import abort
+from flask import redirect
+from flask import make_response
 
 app = Flask(__name__)
 
 @app.route('/user/<name>')
-def hello(name):#attention param name 
+def hello(name):#attention param name
+    if name != 'zhang':
+        abort(404) #
     return '<h1>hello %s</h1>' % name
 
+@app.route('/')
+def index():
+        return redirect('http://www.baidu.com')
+
+@app.route('/resp')
+def resp():
+    response = make_response('<h1>this document carries a cookie</h1>')
+    response.set_cookie('answer', '42')
+    return response
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8080)
